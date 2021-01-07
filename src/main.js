a5e2f10d609c1e0a431a858d74a9651beda6471e
Timothée DURAND <timothee.durand70@gmail.com>
2021-01-07 18:07:30 +0100

    am_integ_simple

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css' // Import precompiled Bootstrap css
+import '@fortawesome/fontawesome-free/css/all.css'
+import './styles/custom-bootstrap.scss'
+
+
+
+
+
