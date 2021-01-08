f7d8d6c19a19578f69e1c34c03aafe150317600d
Timothée DURAND <timothee.durand70@gmail.com>
2021-01-08 09:04:43 +0100

    add index.html, bootstrap-vue and sass-loader

--- boss-s3-disney/src/main.js
+++ boss-s3-disney/src/main.js
@@ -3,6 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+//ajout bootstrap vue
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+
+import "@/styles/custom-bootstrap.scss"
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+
 Vue.config.productionTip = false
 
 new Vue({
